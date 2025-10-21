95705c74d17bbd9d5b8063a9ec28f38f2721a930
Senura Bhawantha <79014951+senurabhawantha@users.noreply.github.com>
2025-10-21 15:08:55 +0530

    Multithreading

--- src/senura/Main.java
+++ src/senura/Main.java
@@ -1,9 +1,18 @@
 package senura;
 
 import senura.exceptions.ExceptionsDemo;
+import senura.threading.Multithreading;
 
 public class Main {
     public static void main(String[] args) {
-        ExceptionsDemo.show();
+        //ExceptionsDemo.show();
+
+        for (int i = 0; i <= 3; i++) {
+            Multithreading multithreading = new Multithreading(i);
+            Thread myThread = new Thread(multithreading);
+            myThread.start();
+        }
+//        throw new RuntimeException();
+
     }
 }
